3bc73d89a8aa7039d971b6e7e3549f6b58166073
Jacob Goeldel <goeldelj@gmail.com>
2023-02-27 10:53:41 -0500

    create source handles for each data column

--- components/nodes/data-node.tsx
+++ components/nodes/data-node.tsx
@@ -1,41 +1,64 @@
-import { Handle, Node, Position } from 'reactflow';
-import { FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, Text } from '@chakra-ui/react';
+import { Handle, Node, Position, useUpdateNodeInternals } from 'reactflow';
+import { Button, FormControl, FormLabel, Input, LightMode, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, Text } from '@chakra-ui/react';
 import DefaultNode from './base-node';
 import Papa from "papaparse";
+import { useState } from 'react';
 
-const handleStyle = { width: 12, height: 12, top: "47%" };
+const handleStyle = { width: 12, height: 12 };
 
 type TextProp = {
   text: string;
 };
 
 const DataNode = (node: Node, data: TextProp) => {
+  const [dataSet, setDataSet] = useState<any | undefined>();
+  const updateNodeInternals = useUpdateNodeInternals();
+
+  const loadData = (e: any) => {
+    const files = e.target.files;
+    if (files) {
+      Papa.parse(files[0], {
+        complete: function (results: any) {
+          setDataSet(results.data);
+          updateNodeInternals(node.id);
+        }
+      });
+    }
+  }
+
+  const onViewData = () => {
+
+  }
+
   return (
     <DefaultNode node={node} data={data} title="Data Upload" titleColor="green.500">
-      <Handle type="source" position={Position.Right} style={handleStyle} />
+
+      {dataSet && dataSet[0].map((c: string, i: number) => (
+        <Handle type="source" position={Position.Right} style={{ ...handleStyle, top: 198 + i * 40 }} key={i} id={`target-handle-${i + 1}`} />
+      ))
+      }
 
       <FormControl>
         <FormLabel color="white">File</FormLabel>
-        <Input 
-        style={{ paddingLeft: 0}}
-        border={0}
-        color="white"
-        type="file"
-        accept=".csv,.xlsx,.xls"
-        onChange={(e) => {
-          const files = e.target.files;
-          console.log(files);
-          if (files) {
-            console.log(files[0]);
-            Papa.parse(files[0], {
-              complete: function(results: any) {
-                console.log("Finished:", results.data);
-              }}
-              )
-            }
-          }}
+        <Input
+          style={{ paddingLeft: 0 }}
+          border={0}
+          color="white"
+          type="file"
+          accept=".csv,.xlsx,.xls"
+          onChange={loadData}
         />
       </FormControl>
+
+      <LightMode>
+        <Button width="full" colorScheme="green" onClick={onViewData}>View Data</Button>
+      </LightMode>
+
+      <>
+        {dataSet && dataSet[0].map((c: string) => (
+          <Text color="white" mt={4} mr={2} textAlign="right">"{c}"</Text>
+        ))}
+      </>
     </DefaultNode>
   );
 }
