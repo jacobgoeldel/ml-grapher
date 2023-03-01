34114091c87f347f69a3a69bf60c2d5f996a1050
Jacob Goeldel <goeldelj@gmail.com>
2023-03-01 13:10:34 -0500

    made formatting consistent

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,19 @@ import Graph from '../components/graph'
 import SideBar from '../components/sidebar'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>ML Grapher</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>ML Grapher</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main style={{ height: '100vh', background: '#181925' }}>
-        <HStack w="100%" h="100%" spacing={0}>
-          <SideBar />
-          <Graph />
-        </HStack>
-      </main>
-    </div>
-  )
+            <main style={{ height: '100vh', background: '#181925' }}>
+                <HStack w="100%" h="100%" spacing={0}>
+                    <SideBar />
+                    <Graph />
+                </HStack>
+            </main>
+        </div>
+    )
 }
