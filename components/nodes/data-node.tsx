6f8e7b8e5f1c638e7d23bf73a494fb9f12750bf1
Jacob Goeldel <goeldelj@gmail.com>
2023-03-01 13:09:09 -0500

    View Data with a Modal Table

--- components/nodes/data-node.tsx
+++ components/nodes/data-node.tsx
@@ -1,66 +1,95 @@
 import { Handle, Node, Position, useUpdateNodeInternals } from 'reactflow';
-import { Button, FormControl, FormLabel, Input, LightMode, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, Text } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, LightMode, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, useDisclosure } from '@chakra-ui/react';
 import DefaultNode from './base-node';
 import Papa from "papaparse";
 import { useState } from 'react';
+import { DataTable } from '../DataTable';
+import { createColumnHelper } from '@tanstack/react-table';
 
 const handleStyle = { width: 12, height: 12 };
 
 type TextProp = {
-  text: string;
+    text: string;
 };
 
 const DataNode = (node: Node, data: TextProp) => {
-  const [dataSet, setDataSet] = useState<any | undefined>();
-  const updateNodeInternals = useUpdateNodeInternals();
+    const [dataSet, setDataSet] = useState<any[][] | undefined>();
+    const [columns, setColumns] = useState<any[]>([]);
+    const updateNodeInternals = useUpdateNodeInternals();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const columnHelper = createColumnHelper<any>();
 
-  const loadData = (e: any) => {
-    const files = e.target.files;
-    if (files) {
-      Papa.parse(files[0], {
-        complete: function (results: any) {
-          setDataSet(results.data);
-          updateNodeInternals(node.id);
+    const loadData = (e: any) => {
+        const files = e.target.files;
+        if (files) {
+            Papa.parse(files[0], {
+                complete: function (results: any) {
+                    console.log(results);
+                    setDataSet(results.data);
+
+                    const newCols = results.meta.fields.map((c: string, i: number) => columnHelper.accessor(c, { header: c, id: `${i}` }));
+                    console.log(newCols);
+                    setColumns(newCols);
+                    updateNodeInternals(node.id);
+                },
+                header: true
+            });
         }
-      });
     }
-  }
 
-  const onViewData = () => {
+    return (
+        <DefaultNode node={node} data={data} title="Data Upload" titleColor="green.500">
 
-  }
+            <>
+                {dataSet && columns.map((c: any, i: number) => (
+                    <Handle type="source" position={Position.Right} style={{ ...handleStyle, top: 198 + i * 40 }} key={i} id={`target-handle-${i + 1}`} />
+                ))
+                }
+            </>
 
-  return (
-    <DefaultNode node={node} data={data} title="Data Upload" titleColor="green.500">
+            <FormControl>
+                <FormLabel color="white">File</FormLabel>
+                <Input
+                    style={{ paddingLeft: 0 }}
+                    border={0}
+                    color="white"
+                    type="file"
+                    accept=".csv,.xlsx,.xls"
+                    onChange={loadData}
+                />
+            </FormControl>
 
-      {dataSet && dataSet[0].map((c: string, i: number) => (
-        <Handle type="source" position={Position.Right} style={{ ...handleStyle, top: 198 + i * 40 }} key={i} id={`target-handle-${i + 1}`} />
-      ))
-      }
+            <>
+                {dataSet &&
+                    <LightMode>
+                        <Button width="full" colorScheme="green" onClick={onOpen}>View Data</Button>
+                    </LightMode>
+                }
+            </>
 
-      <FormControl>
-        <FormLabel color="white">File</FormLabel>
-        <Input
-          style={{ paddingLeft: 0 }}
-          border={0}
-          color="white"
-          type="file"
-          accept=".csv,.xlsx,.xls"
-          onChange={loadData}
-        />
-      </FormControl>
+            <>
+                {dataSet && columns.map((c: any) => (
+                    <Text color="white" mt={4} mr={2} textAlign="right">"{c.header}"</Text>
+                ))}
+            </>
 
-      <LightMode>
-        <Button width="full" colorScheme="green" onClick={onViewData}>View Data</Button>
-      </LightMode>
+            <>
+                {dataSet &&
+                    <Modal isOpen={isOpen} onClose={onClose} isCentered size="6xl" scrollBehavior='inside'>
+                        <ModalOverlay />
+                        <ModalContent>
 
-      <>
-        {dataSet && dataSet[0].map((c: string) => (
-          <Text color="white" mt={4} mr={2} textAlign="right">"{c}"</Text>
-        ))}
-      </>
-    </DefaultNode>
-  );
+                            <ModalHeader color="white">Data Set</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody color="white">
+                                <DataTable columns={columns} data={dataSet} />
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
+                }
+            </>
+        </DefaultNode>
+    );
 }
 
 export default DataNode;
