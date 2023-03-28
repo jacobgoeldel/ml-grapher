e789cf43231bd474365b908b2f717f8966999971
Jacob Goeldel <goeldelj@gmail.com>
2023-03-28 12:09:59 -0400

    training tab with trainer setup

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,9 @@
 import { BiAddToQueue } from 'react-icons/bi'
+import { TbWeight } from 'react-icons/tb'
 import { HStack, IconButton, Icon, VStack } from "@chakra-ui/react";
 import { FC, useState } from "react";
 import AddTab from "./tabs/AddTab";
+import TrainingTab from './tabs/TrainingTab';
 
 const SideBar: FC<{}> = () => {
     const [selectedId, setSelectedId] = useState(-1);
@@ -10,7 +12,7 @@ const SideBar: FC<{}> = () => {
 
     return (
         <HStack h="full" spacing={0}>
-            <VStack p={4} h="full" backgroundColor="gray.700" dropShadow="lg">
+            <VStack p={4} spacing={8} h="full" backgroundColor="gray.700" dropShadow="lg">
                 <IconButton
                     aria-label='Add Nodes'
                     variant="unstyled"
@@ -19,8 +21,17 @@ const SideBar: FC<{}> = () => {
                     color={selectedId == 0 ? "white" : "gray.900"}
                     _hover={{ color: selectedId == 0 ? "white" : "gray.300" }}
                 />
+                <IconButton
+                    aria-label='Training'
+                    variant="unstyled"
+                    icon={<Icon as={TbWeight} w={12} h={12} />}
+                    onClick={() => tabClicked(1)}
+                    color={selectedId == 1 ? "white" : "gray.900"}
+                    _hover={{ color: selectedId == 1 ? "white" : "gray.300" }}
+                />
             </VStack>
             {selectedId == 0 && <AddTab />}
+            {selectedId == 1 && <TrainingTab />}
         </HStack>
     )
 }
---
+++ components/tabs/TrainingTab.tsx
@@ -0,0 +1,171 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, DarkMode, Flex, LightMode, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack, Text, VStack } from "@chakra-ui/react";
+import { FC, useState } from "react";
+import useGraph from "../store";
+import { Trainer } from "convnetjs";
+
+const clamp = (val: number, min: number, max: number) => val < min ? min : (val > max ? max : val);
+
+const TrainingTab: FC<{}> = () => {
+    const { errors, createMLGraph } = useGraph((state) => ({
+        errors: state.errors,
+        createMLGraph: state.createMLGraph
+    }));
+
+    const [method, setMethod] = useState("adadelta");
+    const [learningRate, setLearningRate] = useState(0.01);
+    const [decay, setDecay] = useState(0.001);
+    const [batchSize, setBatchSize] = useState(32);
+    const [epochs, setEpochs] = useState(10);
+    const [trainingSplit, setTrainingSplit] = useState(75);
+
+    const hasErrors = errors.find(e => e.type == "error") != undefined;
+
+    const methodChanged = (evt: any) => setMethod(evt.target.value);
+
+    const startTraining = () => {
+        let net = createMLGraph();
+        console.log("Network", net);
+
+        // shouldn't be possible but just in case
+        if (net == undefined)
+            return;
+
+        const trainerConfig = {
+            method, 
+            learning_rate: method == "sgd" ? learningRate : undefined,
+            l2_decay: decay, 
+            momentum: method == "sgd" ? 0.9 : undefined, 
+            batch_size: batchSize,
+        };
+
+        console.log("Trainer", trainerConfig);
+
+        let trainer = new Trainer(net, trainerConfig);
+    }
+
+    const labelStyles = {
+        mt: '2',
+        ml: '-2.5',
+        fontSize: 'sm',
+    }
+
+    return (
+        <LightMode>
+            <VStack h="full" w="400px" backgroundColor="gray.800" p={8} spacing={12} dropShadow="lg" alignItems="start">
+                <Text fontSize='4xl' color="white" h="min">Model Training</Text>
+
+                <VStack w="full" spacing={4} alignItems="start">
+                    <Text fontSize='xl' color="white" h="min">Hyper Parameters</Text>
+
+                    <Flex alignItems="center" justifyContent="space-between" width="full">
+                        <Text color="white" flex={1}>Method:</Text>
+                        <DarkMode>
+                            <Select color="white" backgroundColor="gray.900" flex={1} value={method} onChange={methodChanged}>
+                                <option value="sgd">SGD</option>
+                                <option value="adadelta">Adadelta</option>
+                                <option value="adagrad">Adagrad</option>
+                            </Select>
+                        </DarkMode>
+                    </Flex>
+
+                    <Flex alignItems="center" justifyContent="space-between" width="full">
+                        <Text color="white" flex={1}>Learning Rate:</Text>
+                        <DarkMode>
+                            <NumberInput max={1} min={0.000001} precision={4} step={0.001} value={learningRate} onChange={(text, val) => setLearningRate(val)} color="white" flex={1} isDisabled={method != "sgd"}>
+                                <NumberInputField backgroundColor="gray.900" />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </DarkMode>
+                    </Flex>
+
+                    <Flex alignItems="center" justifyContent="space-between" width="full">
+                        <Text color="white" flex={1}>Decay:</Text>
+                        <DarkMode>
+                            <NumberInput max={0.9} min={0} precision={4} step={0.001} value={decay} onChange={(text, val) => setDecay(val)} color="white" flex={1}>
+                                <NumberInputField backgroundColor="gray.900" />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </DarkMode>
+                    </Flex>
+
+                    <Flex alignItems="center" justifyContent="space-between" width="full">
+                        <Text color="white" flex={1}>Batch Size:</Text>
+                        <DarkMode>
+                            <NumberInput max={1024} min={1} value={batchSize} onChange={(text, val) => setBatchSize(val)} color="white" flex={1}>
+                                <NumberInputField backgroundColor="gray.900" />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </DarkMode>
+                    </Flex>
+
+                    <Flex alignItems="center" justifyContent="space-between" width="full">
+                        <Text color="white" flex={1}>Epochs:</Text>
+                        <DarkMode>
+                            <NumberInput max={1000} min={1} value={epochs} onChange={(text, val) => setEpochs(val)} color="white" flex={1}>
+                                <NumberInputField backgroundColor="gray.900" />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </DarkMode>
+                    </Flex>
+
+                    <Flex alignItems="center" justifyContent="space-between" width="full" pt={8}>
+                        <Text color="white" flex={1}>Training Split:</Text>
+                        <DarkMode>
+                            <Box pt={6} pb={2} flex={2}>
+                                <Slider aria-label='training-split' value={trainingSplit} onChange={(val) => setTrainingSplit(clamp(val, 10, 90))}>
+                                    <SliderMark value={0} style={labelStyles} color="white" width="full">
+                                        Training
+                                    </SliderMark>
+                                    <SliderMark value={70} style={labelStyles} color="white">
+                                        Validation
+                                    </SliderMark>
+                                    <SliderMark
+                                        value={trainingSplit}
+                                        textAlign='center'
+                                        bg='gray.900'
+                                        rounded="sm"
+                                        color='white'
+                                        mt='-10'
+                                        ml='-5'
+                                        w='12'
+                                    >
+                                        {trainingSplit}%
+                                    </SliderMark>
+                                    <SliderTrack backgroundColor="orange.500">
+                                        <SliderFilledTrack backgroundColor="green.500" />
+                                    </SliderTrack>
+                                    <SliderThumb />
+                                </Slider>
+                            </Box>
+                        </DarkMode>
+                    </Flex>
+                </VStack>
+
+                <VStack w="full" spacing={4} alignItems="start">
+                    <Text fontSize='xl' color="white" h="min">Training</Text>
+                    {hasErrors &&
+                        <Alert status="error" rounded="md">
+                            <AlertIcon />
+                            <AlertTitle>Fix Errors Before Training</AlertTitle>
+                        </Alert>
+                    }
+                    <Button colorScheme="green" width="full" isDisabled={hasErrors} onClick={startTraining}>Start Training</Button>
+                </VStack>
+            </VStack>
+        </LightMode>
+    )
+}
+
+export default TrainingTab;
