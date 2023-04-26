b19aa6df23cf1242297b4649be16564f919d8cb8
Jacob Goeldel <goeldelj@gmail.com>
2023-04-26 15:01:01 -0400

    Adjusted Add tab styling

--- components/tabs/AddTab.tsx
+++ components/tabs/AddTab.tsx
@@ -10,27 +10,27 @@ const AddTab: FC<{ visible: boolean }> = ({ visible }) => {
     return (
         <Box hidden={!visible} h="full">
         <LightMode>
-            <VStack h="full" w="400px" backgroundColor="gray.800" p={8} spacing={12} dropShadow="lg" alignItems="start">
+            <VStack h="full" w="400px" backgroundColor="gray.800" p={12} spacing={12} dropShadow="lg" alignItems="start">
                 <Text fontSize='4xl' color="white" h="min">Nodes</Text>
 
                 <VStack w="full" spacing={4} alignItems="start">
                     <Text fontSize='xl' color="white" h="min">Data Nodes</Text>
-                    <Button colorScheme="green" onDragStart={(event) => onDragStart(event, 'dataNode')} draggable width="full">Data Upload</Button>
-                    <Button colorScheme="green" onDragStart={(event) => onDragStart(event, 'tokenizerNode')} draggable width="full">Tokenizer</Button>
-                    <Button colorScheme="green" onDragStart={(event) => onDragStart(event, 'normalizerNode')} draggable width="full">Normalize</Button>
-                    <Button colorScheme="green" onDragStart={(event) => onDragStart(event, 'onehotNode')} draggable width="full">One Hot Encoder</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="green" onDragStart={(event) => onDragStart(event, 'dataNode')} draggable width="full" cursor="grab">Data Upload</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="green" onDragStart={(event) => onDragStart(event, 'tokenizerNode')} draggable width="full" cursor="grab">Tokenizer</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="green" onDragStart={(event) => onDragStart(event, 'normalizerNode')} draggable width="full" cursor="grab">Normalize</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="green" onDragStart={(event) => onDragStart(event, 'onehotNode')} draggable width="full" cursor="grab">One Hot Encoder</Button>
                 </VStack>
 
                 <VStack w="full" spacing={4} alignItems="start">
                     <Text fontSize='xl' color="white" h="min">Neural Network Nodes</Text>
-                    <Button colorScheme="red" onDragStart={(event) => onDragStart(event, 'inputNode')} draggable width="full">Input Layer</Button>
-                    <Button colorScheme="red" onDragStart={(event) => onDragStart(event, 'denseNode')} draggable width="full">Dense Layer</Button>
-                    <Button colorScheme="red" onDragStart={(event) => onDragStart(event, 'outputNode')} draggable width="full">Output Layer</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="red" onDragStart={(event) => onDragStart(event, 'inputNode')} draggable width="full" cursor="grab">Input Layer</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="red" onDragStart={(event) => onDragStart(event, 'denseNode')} draggable width="full" cursor="grab">Dense Layer</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="red" onDragStart={(event) => onDragStart(event, 'outputNode')} draggable width="full" cursor="grab">Output Layer</Button>
                 </VStack>
 
                 <VStack w="full" spacing={4} alignItems="start">
                     <Text fontSize='xl' color="white" h="min">Utility Nodes</Text>
-                    <Button colorScheme="blue" onDragStart={(event) => onDragStart(event, 'commentNode')} draggable width="full">Comment</Button>
+                    <Button justifyContent="flex-start" borderRadius='sm' colorScheme="blue" onDragStart={(event) => onDragStart(event, 'commentNode')} draggable width="full" cursor="grab">Comment</Button>
                 </VStack>
             </VStack>
         </LightMode>
